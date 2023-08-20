7de6920f497a0b4a6aa32684e1292df6e467127f
PRADIP BHATT <bhattsammar04@gmail.com>
2023-08-20 07:12:08 +0545

    Commiting the first changes

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Hello world");
+// For running this file,use node main.js in terminal 
+const a=10;
+const b=30;
+c=a+b;
+console.log(c);
---
+++ multiplication.js
@@ -0,0 +1,4 @@
+const a=10;
+const b=20;
+multiplication=a*b;
+console.log("The multiplication of the two numbers=",multiplication);
