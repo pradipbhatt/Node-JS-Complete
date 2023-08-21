4e2687d150649ffe4b136bd209772e65feb75c2a
PRADIP BHATT <bhattsammar04@gmail.com>
2023-08-21 10:07:52 +0545

    Day 2 for commiting abt global objects

---
+++ Day-2/GlobalObjects.js
@@ -0,0 +1,19 @@
+//console is global object which is predefined object 
+//__dirname   (file ko path lae get garna ko lagi)
+//__filename
+//require  
+//buffer,   (temporary value include garcha value pass gareko lae store garcha)
+//module,
+//exports,
+
+console.log(__dirname); //gives path of current directory
+console.log(__filename); //gives path of current file
+//require is used to import module
+var main=require("./Object.js");   //importing module of object.js
+
+var pradip=require("./Anonymous.js");
+
+
+
+
+//buffer is used to store temporary data
